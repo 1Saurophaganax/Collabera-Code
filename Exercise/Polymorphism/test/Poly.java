aef8215ff9b6a9d99369ffe1b27fc2e3d0787e30
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-08-16 09:22:43 -0400

    Polymorphism test

---
+++ Exercise/Polymorphism/test/Poly.java
@@ -0,0 +1,48 @@
+
+class Person {
+
+    protected String ssn;
+ 
+    void generateSSN(String s){
+        ssn = s;
+    }
+    public String getssn() {
+        return ssn;
+    }
+ }
+ 
+ class Japan extends Person{
+ 
+     void generateSSN(String s){
+         ssn = "Japan " + s;
+     }
+ 
+ }
+ 
+ class US extends Person{
+ 
+     void generateSSN(String s){
+         ssn = "US " + s;
+     }
+ 
+ }
+ 
+ 
+ 
+ class Poly{
+     public static void main(String[] args) {
+ 
+     US Josiah = new US();
+     Japan Jotaro = new Japan();
+     Japan Jolyne = new Japan();
+ 
+     Josiah.generateSSN("123");
+     Jotaro.generateSSN("2384");
+     Jolyne.generateSSN("353");
+ 
+     System.out.println("Person Josukes ssn is " + Josiah.getssn());
+     System.out.println("Person Jolyne's ssn is " + Jolyne.getssn());
+     System.out.println("Person Jotaro's ssn is " + Jotaro.getssn());
+ 
+     }
+ }
